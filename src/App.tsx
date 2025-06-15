59c0f0becafaa2dc36013e796c3197bcd6d7d3c6
kemickoko <kemickokojiji.work@gmail.com>
2025-06-15 16:18:48 +0900

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>ようこそ、私のホームページへ！</h1>
+      <p>React + Vite + Vercelで作りました。</p>
+    </div>
+  );
+}
+export default App;
