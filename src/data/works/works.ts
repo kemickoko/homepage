afc0c90c9f5f5424b78c31b2bd576eb88e6eb2b6
kemickoko <kemickokojiji.work@gmail.com>
2025-07-02 10:53:53 +0900

    githubリンクを作成

--- src/data/works/works.ts
+++ src/data/works/works.ts
@@ -3,31 +3,36 @@ export type WorkItem = {
   title: string;
   description: string;
   link: string;
+  github?: string;
 };
 
 export const initialWorks: WorkItem[] = [
   {
     id: "1",
-    title: "ミニゲーム",
-    description: "コインの合計金額を当てるミニゲーム",
-    link: "https://coin-game-kappa.vercel.app/",
+    title: "ホームページ",
+    description: "このサイトです",
+    link: "https://homepage-xi-indol.vercel.app/",
+    github: "https://github.com/kemickoko/homepage",
   },
   {
     id: "2",
-    title: "制作物２",
-    description: "制作物２",
-    link: "2",
+    title: "ミニゲーム",
+    description: "コインの合計金額を当てるミニゲーム",
+    link: "https://coin-game-kappa.vercel.app/",
+    github: "https://github.com/kemickoko/coin-game",
   },
   {
     id: "3",
     title: "制作物３",
     description: "制作物３",
     link: "3",
+    github: "2",
   },
   {
     id: "4",
     title: "制作物４",
     description: "制作物４",
     link: "4",
+    github: "2",
   },
 ];
