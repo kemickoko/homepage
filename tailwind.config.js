e415e6f932388db3f29072c24230ac03be70bed0
kemickoko <kemickokojiji.work@gmail.com>
2025-07-02 10:36:58 +0900

    フォントの変更

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,7 @@ export default {
   theme: {
     extend: {
       fontFamily: {
-        sans: ['Inter', 'sans-serif'],
+        Kosugi: ['Kosugi', 'sans-serif'],
       },
       colors: {
         brand: {
